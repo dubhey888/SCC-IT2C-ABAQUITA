a974ed1eae3f197889f2935a303522cc837b6540
abaquitadubhey <abaquitadubhey@gmail.com>
2024-09-10 11:07:43 +0800

    hello

--- src/abaquita/Abaquita.java
+++ src/abaquita/Abaquita.java
@@ -6,11 +6,30 @@ public class Abaquita {
     public static void main(String[] args) {
 
         
-   salaries salaries = new salaries();
-   salaries.totalsalary();  
+//   salaries salaries = new salaries();
+//   salaries.totalsalary();  
    
-   accounts accountManager = new accounts();
-   accountManager.manageAccounts();
+//   accounts accountManager = new accounts();
+//   accountManager.manageAccounts();
+
+   
+   
+//         grades gr = new grades();
+//         gr.addGrades(1011,"dubhey", 1.2, 1.4, 1.3, 1.2);
+//         gr.viewGrades();
+
+//        netpay np = new netpay();
+//        np.getpay();
+
+
+//        
+//        product pr = new product();
+//        pr.addProduct(10011,"soap", 20, 30, 20);
+//        pr.viewProduct();
+        
+        
+
+
 
        
     }
